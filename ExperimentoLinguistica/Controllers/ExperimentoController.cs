e2a4f79b108d48943fea0f420afe77c4dde6ad84
Rafael Lins Pinheiro <rafael.pinheiro@granadophebo.com.br>
2024-09-25 15:30:41 -0300

    Inicial

---
+++ ExperimentoLinguistica/Controllers/ExperimentoController.cs
@@ -0,0 +1,58 @@
+﻿using ExperimentoLinguistica.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ExperimentoLinguistica.Controllers
+{
+    public class ExperimentoController : Controller
+    {
+        public IActionResult BoasVindas()
+        {
+            return View();
+        }
+
+        public IActionResult Consentimento()
+        {
+            return View(new Consentimento());
+        }
+
+        [HttpPost]
+        public IActionResult Consentimento(Consentimento model)
+        {
+            if (model.Aceito)
+            {
+                return RedirectToAction("Instrucoes");
+            }
+            else
+            {
+                return RedirectToAction("BoasVindas");
+            }
+        }
+
+        public IActionResult Instrucoes()
+        {
+            return View();
+        }
+
+        public IActionResult Experimento()
+        {
+            var estimulos = ObterEstimulos();
+            return View(estimulos);
+        }
+
+        private List<Estimulo> ObterEstimulos()
+        {
+            return new List<Estimulo>
+        {
+            new Estimulo { Prime = "Pincel", Alvo = "苹果", TempoPrime = 50, TempoAlvo = 3000 },
+            new Estimulo { Prime = "Café", Alvo = "家具", TempoPrime = 50, TempoAlvo = 3000 },
+
+        };
+        }
+
+        public IActionResult Final()
+        {
+            return View();
+        }
+    }
+
+}
---
+++ ExperimentoLinguistica/Models/Estimulo.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExperimentoLinguistica.Models
+{
+    public class Estimulo
+    {
+        public string Prime { get; set; }
+        public string Alvo { get; set; }
+        public int TempoPrime { get; set; }
+        public int TempoAlvo { get; set; }
+    }
+}
