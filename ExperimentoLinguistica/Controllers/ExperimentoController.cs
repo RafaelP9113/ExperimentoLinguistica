39b7ffb9f9f8e6aaa1c2a54e566b8ab189a4c9f3
Rafael Lins Pinheiro <rafael.pinheiro@granadophebo.com.br>
2024-10-02 11:47:56 -0300

    Salvando audios em pasta local

--- ExperimentoLinguistica/Controllers/ExperimentoController.cs
+++ ExperimentoLinguistica/Controllers/ExperimentoController.cs
@@ -1,10 +1,15 @@
 ﻿using ExperimentoLinguistica.Models;
 using Microsoft.AspNetCore.Mvc;
+using NPOI.SS.UserModel;
+using NPOI.XSSF.UserModel;
+using System.Collections.Generic;
 
 namespace ExperimentoLinguistica.Controllers
 {
     public class ExperimentoController : Controller
     {
+        private readonly string _audioDirectory = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot", "audios");
+
         public IActionResult BoasVindas()
         {
             return View();
@@ -52,6 +57,102 @@ namespace ExperimentoLinguistica.Controllers
         public IActionResult Final()
         {
             return View();
+        }
+
+
+        [HttpGet]
+        public IActionResult ObterTextos(string diretorio)
+        {
+            string filePath = string.Empty;
+
+            if (diretorio == "Treino")
+            {
+                filePath = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot", "treino.xlsx");
+            }
+            else
+            {
+                filePath = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot", "lista.xlsx");
+            }
+
+            var textos = new List<string[]>();
+
+            using (var stream = new FileStream(filePath, FileMode.Open, FileAccess.Read))
+            {
+                IWorkbook workbook = new XSSFWorkbook(stream);
+                ISheet sheet = workbook.GetSheetAt(0);
+                int rowCount = sheet.LastRowNum;
+
+                for (int rowIndex = 1; rowIndex <= rowCount; rowIndex++)
+                {
+                    IRow row = sheet.GetRow(rowIndex);
+
+                    if (row != null)
+                    {
+                        string texto = row.GetCell(0)?.ToString();  
+                        string simbolo1 = row.GetCell(1)?.ToString();
+                        string simbolo2 = row.GetCell(2)?.ToString();
+                        string exemplo = row.GetCell(3)?.ToString();
+
+                        textos.Add(new string[] { texto, simbolo1, simbolo2, exemplo });
+                    }
+                }
+            }
+
+            return Json(textos);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> SalvarAudio(string diretorio)
+        {
+            var file = Request.Form.Files[0];
+            var frase = int.Parse(Request.Form["frase"]);
+
+            if (file != null && file.Length > 0)
+            {
+                string fileName = GerarNomeArquivo(frase, diretorio);
+                string filePath = Path.Combine(_audioDirectory, fileName);
+
+                using (var stream = new FileStream(filePath, FileMode.Create))
+                {
+                    await file.CopyToAsync(stream);
+                }
+
+                return Ok(new { mensagem = "Áudio salvo com sucesso.", nomeArquivo = fileName });
+            }
+
+            return BadRequest("Falha ao receber o arquivo de áudio.");
+        }
+
+        private string GerarNomeArquivo(int frase,string diretorio)
+        {
+            if (!Directory.Exists(_audioDirectory))
+            {
+                Directory.CreateDirectory(_audioDirectory);
+            }
+
+            string sufixo = diretorio == "Treino" ? "T" : "E";
+            var arquivosExistentes = Directory.GetFiles(_audioDirectory, $"P*F{frase}{sufixo}.wav");
+
+            int maiorP = 0;
+
+            foreach (var arquivo in arquivosExistentes)
+            {
+                var nomeArquivo = Path.GetFileNameWithoutExtension(arquivo);
+
+                var partes = nomeArquivo.Split('P', 'F', sufixo[0]);
+
+                if (partes.Length >= 2 && int.TryParse(partes[1], out int numeroP))
+                {
+                    if (numeroP > maiorP)
+                    {
+                        maiorP = numeroP;
+                    }
+                }
+            }
+
+            int proximoP = maiorP + 1;
+
+            return $"P{proximoP}F{frase}{sufixo}.wav";
         }
     }
 
